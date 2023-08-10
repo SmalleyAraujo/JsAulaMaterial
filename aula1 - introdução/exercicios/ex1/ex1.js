43bc7914009c4837d15b1e257228a4aa5ae9a59d
SmalleyAraujo <133250347+SmalleyAraujo@users.noreply.github.com>
2023-08-10 18:03:01 -0300

    v1.1.2

--- aula1 - introdução/exercicios/ex1/ex1.js
+++ aula1 - introdução/exercicios/ex1/ex1.js
@@ -1,8 +1,9 @@
 window.alert(" Bem vindo(a) calcula conta");
-let valCont = parseFloat(window.prompt("informe o valor da conta:"));
-let quantP = parseInt(window.prompt("Informe a quantidade de pessoas:"));
+const valCont = Number(window.prompt("informe o valor da conta:"));
+const quantP = Number(window.prompt("Informe a quantidade de pessoas:"));
+const adicionais = Number (window.prompt("Quanto de adicional:"));
 
 
-const totUnit = valCont / quantP;
+const totUnit = (valCont + adicionais)/quantP.toFixed(2);
 
 window.alert(`O valor da conta por cliente é: ${totUnit}`);
--- aula1 - introdução/exercicios/ex2/ex2.js
+++ aula1 - introdução/exercicios/ex2/ex2.js
@@ -1,7 +1,7 @@
 window.alert(" Bem vindo(a) a calculadora sucessor e antecessor");
-let num1 = parseInt(window.prompt("Digite um número:"));
+let num1 = parseInt(window.prompt("Qual é o seu número escolhido?"));
 
-const suces = num1 + 1;
-const antec = num1 - 1;
+// const suces = num1 + 1;
+// const antec = num1 - 1;
 
-window.alert(`O sucessor é: ${suces} e o antecessor é: ${antec}`);
+window.alert(`O sucessor é: ${num1 + 1} e o antecessor é: ${num1 -1}`);
