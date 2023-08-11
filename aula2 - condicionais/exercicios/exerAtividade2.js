1dc9b5ef5ffde9cb77ce0167b678f3106aeed62f
SmalleyAraujo <133250347+SmalleyAraujo@users.noreply.github.com>
2023-08-11 17:51:45 -0300

    Update exerAtividade2.js

--- aula2 - condicionais/exercicios/exerAtividade2.js
+++ aula2 - condicionais/exercicios/exerAtividade2.js
@@ -26,3 +26,34 @@ if (idade < 0 || isNaN(idade)){
         
     }
 }
+
+//OUTRA FORMA DE RESOLUÇÃO DA QUESTÃO
+// window.alert("Boa tarde, classificação por faixa etária de idade");
+// const idade = Number(window.prompt("Informe a sua idade: "));
+ 
+// if( idade < 0){
+//     window.alert("Idade Inválida, insira novamente a sua idade");
+//     location.reload();
+// }
+
+// if( idade >= 0 & idade < 15){
+//     window.alert("Usuário da faixa etária criança");
+//     location.reload();
+// }
+
+// if( idade >= 15 & idade < 30){
+//     window.alert("Usuário da faixa etária  jovem");
+//     location.reload();
+// }
+
+// if( idade >= 30 & idade < 60){
+//     window.alert("Usuário da faixa etária  adulto");
+//     location.reload();
+// }
+
+// if( idade >= 60){
+//     window.alert("Usuário da faixa etária  idoso");
+//     location.reload();
+// }
+
+
