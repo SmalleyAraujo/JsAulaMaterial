a467c6f65d1647f73e4ce1d416042df67da37c55
SmalleyAraujo <133250347+SmalleyAraujo@users.noreply.github.com>
2023-08-26 01:01:53 -0300

    Update

--- aula11-Jquery/exerciciosAtividades9 -jquery/Js/scriptExercicio1.js
+++ aula11-Jquery/exerciciosAtividades9 -jquery/Js/scriptExercicio1.js
@@ -5,23 +5,38 @@ var operadorSelecionado = document.querySelector("#operador");
 //Eventos = addEventLintener 
 
 $("#calcular").on("click", function(){
-     let numeroValue = $("#numero").value;
+
+     var numeroValueInf = numeroValue.value;
      var operadorSelecionadoVal = operadorSelecionado.value;
       
 
      switch (operadorSelecionadoVal) {
           case "soma":
                for(let contador = 1; contador <= 10; contador++){
-                    resultadoDiv.innerHTML += `${contador} + ${numeroValue} = ${contador + numeroValue}<br>`
-                }
-               break;
-          case "subtracao":
-               for(let contador = 1; contador <= 10; contador++){
-                    resultadoDiv.innerHTML += `${contador} - ${numeroValue} = ${contador - numeroValue}<br>`
+                    resultadoDiv.innerHTML += `${numeroValueInf} + ${contador} = ${parseInt(contador) + parseInt(numeroValueInf)}<br>`
                 }
                break;
 
+          case "subtracao":
+               for(let contador = 1; contador <= 10; contador++){
+                    resultadoDiv.innerHTML += `${numeroValueInf} - ${contador} = ${numeroValueInf - contador}<br>`
+                }
+               break;
+
+          case "multiplicacao":
+               for(let contador = 1; contador <= 10; contador++){
+                    resultadoDiv.innerHTML += `${numeroValueInf} * ${contador} = ${numeroValueInf * contador}<br>`
+                }
+               break;
+
+          case "divisao":
+               for(let contador = 1; contador <= 10; contador++){
+                    resultadoDiv.innerHTML += `${numeroValueInf} / ${contador} = ${numeroValueInf / contador}<br>`
+                }
+               break;          
+
           default:
+               
                break;
      }
      
